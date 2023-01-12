7ef819b3daf8d3731cd1e5609ec5c15cb77c119d
謝彧麒 <lise84862@gmail.com>
2023-01-12 11:25:35 +0800

    錯誤修改

--- InternalSystem/Models/PersonnelLeaveForm.cs
+++ InternalSystem/Models/PersonnelLeaveForm.cs
@@ -7,27 +7,27 @@ namespace InternalSystem.Models
 {
     public partial class PersonnelLeaveForm
     {
-        public PersonnelLeaveForm()
-        {
-            PersonnelLeaveFormConectStatuses = new HashSet<PersonnelLeaveFormConectStatus>();
-        }
-
         public int LeaveId { get; set; }
         public int EmployeeId { get; set; }
         public int StatusId { get; set; }
         public int LeaveType { get; set; }
         public DateTime StartDate { get; set; }
-        public TimeSpan StartTime { get; set; }
+        public string StartTime { get; set; }
         public DateTime EndDate { get; set; }
-        public TimeSpan EndTime { get; set; }
+        public string EndTime { get; set; }
         public int Proxy { get; set; }
         public int AuditManerger { get; set; }
         public int TotalTime { get; set; }
         public string Reason { get; set; }
+        public bool? ProxyAudit { get; set; }
+        public DateTime? ProxyAuditDate { get; set; }
+        public bool? ManagerAudit { get; set; }
+        public DateTime? ManagerAuditDate { get; set; }
+        public string AuditOpnion { get; set; }
 
         public virtual PersonnelProfileDetail Employee { get; set; }
         public virtual PersonnelLeaveType LeaveTypeNavigation { get; set; }
         public virtual PersonnelProfileDetail ProxyNavigation { get; set; }
-        public virtual ICollection<PersonnelLeaveFormConectStatus> PersonnelLeaveFormConectStatuses { get; set; }
+        public virtual PersonnelLeaveAuditStatus Status { get; set; }
     }
 }
