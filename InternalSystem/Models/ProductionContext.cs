12c749269fb24198c476ff785c99c2a8478a1849
謝彧麒 <lise84862@gmail.com>
2023-01-12 10:41:53 +0800

    錯誤更正

--- InternalSystem/Models/ProductionContext.cs
+++ InternalSystem/Models/ProductionContext.cs
@@ -10,23 +10,11 @@ namespace InternalSystem.Models
         public int OrderId { get; set; }
         public int EmployeeId { get; set; }
         public DateTime Date { get; set; }
-<<<<<<< HEAD
-=======
-
->>>>>>> 2ac6ade0fe0d4d294f96a489921b1fcc34a7d002
         public int ProcessId { get; set; }
         public int AreaId { get; set; }
         public string StartTime { get; set; }
         public string EndTime { get; set; }
-<<<<<<< HEAD
-        public TimeSpan StartTime { get; set; }
-        public TimeSpan EndTime { get; set; }
-=======
-
-
->>>>>>> 2ac6ade0fe0d4d294f96a489921b1fcc34a7d002
         public string Context { get; set; }
-
         public virtual PersonnelProfileDetail Employee { get; set; }
         public virtual ProductionProcessList ProductionProcessList { get; set; }
     }
