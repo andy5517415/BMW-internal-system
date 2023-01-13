29a84da4eb8f3ed2d582c866d12c671a319b3a9b
andy5517415 <andy5517415@gmail.com>
2023-01-13 15:08:24 +0800

    登入器Session綁nav 1

---
+++ InternalSystem/wwwroot/assets/js/LoginSession.js
@@ -0,0 +1,32 @@
+
+var EmpNumber = JSON.parse(sessionStorage.getItem("EmpNumber", JSON.stringify(EmpNumber)));
+var DepID = JSON.parse(sessionStorage.getItem("DepID", JSON.stringify(DepID)));
+var Name = JSON.parse(sessionStorage.getItem("Name", JSON.stringify(Name)));
+
+switch (DepID) {
+  case 1:
+    Dep = "人事部";
+    break;
+
+  case 2:
+    Dep = "生產部";
+    break;
+  case 3:
+    Dep = "業務部";
+    break;
+  case 1:
+    Dep = "採購部";
+    break;
+
+  default:
+    break;
+}
+
+var navInf = new Vue({
+  el: '#navInf',
+  data: {
+    EmpNumber: EmpNumber,
+    Name: Name,
+    Dep: Dep
+  }
+})
