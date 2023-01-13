eff05c08fd3292833539a26a567c96d9fff273a3
andy5517415 <andy5517415@gmail.com>
2023-01-13 19:39:09 +0800

    登入器Session綁nav(1/2)

--- InternalSystem/wwwroot/assets/js/LoginSession.js
+++ InternalSystem/wwwroot/assets/js/LoginSession.js
@@ -21,7 +21,7 @@ switch (DepID) {
   default:
     break;
 }
-
+/*
 var navInf = new Vue({
   el: '#navInf',
   data: {
@@ -30,3 +30,18 @@ var navInf = new Vue({
     Dep: Dep
   }
 })
+*/
+
+const navInf = {
+  data() {
+    return {
+      navInf: {
+        EmpNumber: EmpNumber,
+        Name: Name,
+        Dep: Dep
+      }
+    }
+  }
+}
+
+Vue.createApp(navInf).mount('#navInf')
