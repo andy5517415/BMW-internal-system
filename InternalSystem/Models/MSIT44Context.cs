b3b5ce4e21acee0ea24a800b1e67985a43cadc39
Jeff <jeff82016@gmail.com>
2023-01-18 03:32:24 +0800

    新增報工檢查頁面(還未寫判斷) 調整報工頁面的Vue

--- InternalSystem/Models/MSIT44Context.cs
+++ InternalSystem/Models/MSIT44Context.cs
@@ -52,11 +52,11 @@ namespace InternalSystem.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-<<<<<<< HEAD
+
                 optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=MSIT44;Integrated Security=True;");
-=======
+
                 optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=MSIT44;Integrated Security=True;");
->>>>>>> d2f543753ea79eb95ea16ff0f0173004b50cdfc2
+
             }
         }
 
