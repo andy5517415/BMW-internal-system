7d1fdcfd1f42ce993dadbb6985b008868cd97b54
LIYAOLI-TM56NHA\Administrator <adsf8564@gmail.com>
2023-01-22 01:21:42 +0800

    將請假審核  申請之狀態更改  申請核准時間移往控制器

--- InternalSystem/Controllers/PersonnelLeaveFormsController.cs
+++ InternalSystem/Controllers/PersonnelLeaveFormsController.cs
@@ -9,6 +9,7 @@ using InternalSystem.Models;
 using System.Security.Cryptography.X509Certificates;
 using Newtonsoft.Json;
 using Microsoft.CodeAnalysis;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace InternalSystem.Controllers
 {
@@ -59,7 +60,7 @@ namespace InternalSystem.Controllers
                                          pl.ProxyAudit,
                                          pl.ManagerAudit,
                                          ProxyAuditDate = pl.ProxyAuditDate.ToString(),
-                                         ManagerAuditDate =  pl.ManagerAuditDate.ToString(),
+                                         ManagerAuditDate = pl.ManagerAuditDate.ToString(),
                                          Proxy = pl.Proxy,
                                          auditManerger = pl.AuditManerger,
                                          o.PositionId,
@@ -160,14 +161,14 @@ namespace InternalSystem.Controllers
         //GET被指定代理人之申請
         // GET: api/PersonnelLeaveForms/5
         [HttpGet("proxyAudit/{depId}/{position}/{id}")]
-        public async Task<ActionResult<dynamic>> ProxyLeaveForm(int depId , int position , int Id)
+        public async Task<ActionResult<dynamic>> ProxyLeaveForm(int depId, int position, int Id)
         {
             var personnelLeaveForm = from pl in _context.PersonnelLeaveForms
                                      join o in _context.PersonnelProfileDetails on pl.EmployeeId equals o.EmployeeId
                                      join l in _context.PersonnelLeaveAuditStatuses on pl.StatusId equals l.StatusId
                                      join d in _context.PersonnelDepartmentLists on o.DepartmentId equals d.DepartmentId
                                      join lt in _context.PersonnelLeaveTypes on pl.LeaveType equals lt.LeaveTypeId
-                                     where o.DepartmentId == depId && o.PositionId == position && pl.Proxy == Id 
+                                     where o.DepartmentId == depId && o.PositionId == position && pl.Proxy == Id
                                      && pl.StatusId == 1
                                      orderby pl.LeaveId descending
                                      select new
@@ -215,7 +216,7 @@ namespace InternalSystem.Controllers
                                      join l in _context.PersonnelLeaveAuditStatuses on pl.StatusId equals l.StatusId
                                      join d in _context.PersonnelDepartmentLists on o.DepartmentId equals d.DepartmentId
                                      join lt in _context.PersonnelLeaveTypes on pl.LeaveType equals lt.LeaveTypeId
-                                     where o.DepartmentId == depId  && pl.StatusId == 2
+                                     where o.DepartmentId == depId && pl.StatusId == 2
                                      orderby pl.LeaveId descending
                                      select new
                                      {
@@ -256,7 +257,7 @@ namespace InternalSystem.Controllers
         //員工GET請假申請退件(代理人不同意or主管不同意)
         // GET: api/PersonnelLeaveForms/5
         [HttpGet("retrun/{depId}/{eid}")]
-        public async Task<ActionResult<dynamic>> LeaveReturnForm(int depId,int eid)
+        public async Task<ActionResult<dynamic>> LeaveReturnForm(int depId, int eid)
         {
             var personnelLeaveForm = from pl in _context.PersonnelLeaveForms
                                      join o in _context.PersonnelProfileDetails on pl.EmployeeId equals o.EmployeeId
@@ -279,9 +280,9 @@ namespace InternalSystem.Controllers
                                          LeaveType = pl.LeaveType,
                                          StatusId = pl.StatusId,
                                          pl.ProxyAudit,
-                                         ProxyAuditDate =  pl.ProxyAuditDate.ToString(),
+                                         ProxyAuditDate = pl.ProxyAuditDate.ToString(),
                                          pl.ManagerAudit,
-                                         ManagerAuditDate= pl.ManagerAuditDate.ToString(),
+                                         ManagerAuditDate = pl.ManagerAuditDate.ToString(),
                                          AuditStatus = l.AuditStatus,
                                          Proxy = pl.Proxy,
                                          auditManerger = pl.AuditManerger,
@@ -307,7 +308,7 @@ namespace InternalSystem.Controllers
             var personnelLeaveForm = from ap in _context.PcApplications
                                      join pd in _context.PersonnelProfileDetails on ap.EmployeeId equals pd.EmployeeId
                                      join pod in _context.PcOrderDetails on ap.OrderId equals pod.OrderId
-                                     where ap.EmployeeId == id && ap.ApplicationRejectStatus ==false
+                                     where ap.EmployeeId == id && ap.ApplicationRejectStatus == false
 
                                      select new
                                      {
@@ -361,7 +362,7 @@ namespace InternalSystem.Controllers
                            Quantiy = OD.Quantiy,
                            UnitPrice = OD.UnitPrice,
                            Subtotal = OD.Subtotal,
-                           ProductId =  OD.ProductId
+                           ProductId = OD.ProductId
                        };
 
 
@@ -382,14 +383,19 @@ namespace InternalSystem.Controllers
             return personnelLeaveForm;
         }
 
+
         // PUT: api/PersonnelLeaveForms/5
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPut("{id}")]
-        public async Task<IActionResult> PutPersonnelLeaveForm(int id, PersonnelLeaveForm personnelLeaveForm)
+        public async Task<IActionResult> PutPersonnelLeaveFormForManager(int id, PersonnelLeaveForm personnelLeaveForm)
         {
             if (id != personnelLeaveForm.LeaveId)
             {
                 return BadRequest();
+            }
+            else if (personnelLeaveForm.StatusId != 2)
+            {
+                return NotFound();
             }
 
             _context.Entry(personnelLeaveForm).State = EntityState.Modified;
@@ -413,18 +419,186 @@ namespace InternalSystem.Controllers
             return NoContent();
         }
 
+
+        //代理人同意
+        // PUT: api/PersonnelLeaveForms/proxy/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("proxy/{id}")]
+        public void PutLeaveProxyForm([FromBody]PersonnelLeaveForm personnelLeaveForm)
+        {
+            var proxydate = DateTime.Now;
+            var update = (from a in _context.PersonnelLeaveForms
+                          where a.LeaveId== personnelLeaveForm.LeaveId
+                          select a).SingleOrDefault();
+
+            if(update != null)
+            {
+                update.StatusId = 2;
+                update.ProxyAudit = true;
+                update.ProxyAuditDate = proxydate;
+                _context.SaveChanges();
+            }
+
+            
+        }
+
+        //代理人拒絕
+        // PUT: api/PersonnelLeaveForms/proxy/refuse/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("proxy/refuse/{id}")]
+        public void PutLeaveProxyRefuseForm([FromBody] PersonnelLeaveForm personnelLeaveForm)
+        {
+            var proxydate = DateTime.Now;
+            var update = (from a in _context.PersonnelLeaveForms
+                          where a.LeaveId == personnelLeaveForm.LeaveId
+                          select a).SingleOrDefault();
+
+            if (update != null)
+            {
+                update.StatusId = 3;
+                update.ProxyAudit = false;
+                update.ProxyAuditDate = proxydate;
+                _context.SaveChanges();
+            }
+
+        }
+
+        //主管同意
+        // PUT: api/PersonnelLeaveForms/manager/5
+        [HttpPut("manager/auditleave/{id}")]
+        public void PutLeaveManagerForm([FromBody] PersonnelLeaveForm personnelLeaveForm)
+        {
+            var managerdate = DateTime.Now;
+
+            var update = (from a in _context.PersonnelLeaveForms
+                          where a.LeaveId == personnelLeaveForm.LeaveId
+                          select a).SingleOrDefault();
+
+            if (update != null)
+            {
+                update.StatusId = 4;
+                update.ManagerAudit = true;
+                update.ManagerAuditDate = managerdate;
+                update.AuditOpnion = personnelLeaveForm.AuditOpnion;
+                _context.SaveChanges();
+            }
+        }
+
+        //主管不同意
+        // PUT: api/PersonnelLeaveForms/manager/refuse/5
+        [HttpPut("manager/Refuse/{id}")]
+        public void PutLeaveManagerRefuseForm([FromBody] PersonnelLeaveForm personnelLeaveForm)
+        {
+            var managerdate = DateTime.Now;
+
+            var update = (from a in _context.PersonnelLeaveForms
+                          where a.LeaveId == personnelLeaveForm.LeaveId
+                          select a).SingleOrDefault();
+
+            if (update != null)
+            {
+                update.StatusId = 5;
+                update.ManagerAudit = false;
+                update.ManagerAuditDate = managerdate;
+                update.AuditOpnion = personnelLeaveForm.AuditOpnion;
+                _context.SaveChanges();
+            }
+        }
+
+
+      
+        //// PUT: api/PersonnelLeaveForms/5
+        //// To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> PutPersonnelLeaveForm(int id, PersonnelLeaveForm personnelLeaveForm)
+        //{
+        //    if (id != personnelLeaveForm.LeaveId)
+        //    {
+        //        return BadRequest();
+        //    }
+
+        //    _context.Entry(personnelLeaveForm).State = EntityState.Modified;
+
+        //    try
+        //    {
+        //        await _context.SaveChangesAsync();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!PersonnelLeaveFormExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
+
+        //    return NoContent();
+        //}
+
+        //申請請假
         // POST: api/PersonnelLeaveForms
         // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
         [HttpPost]
-        public async Task<ActionResult<PersonnelLeaveForm>> PostPersonnelLeaveForm(PersonnelLeaveForm personnelLeaveForm)
+        public void PostPersonnalLeaveForm([FromBody]PersonnelLeaveForm personnelLeaveForm) 
         {
-            _context.PersonnelLeaveForms.Add(personnelLeaveForm);
-            await _context.SaveChangesAsync();
+            var application = DateTime.Now.ToString("yyyy-MM-dd");
 
-            return CreatedAtAction("GetPersonnelLeaveForm", new { id = personnelLeaveForm.LeaveId }, personnelLeaveForm);
+            PersonnelLeaveForm insert = new PersonnelLeaveForm
+            {
+                EmployeeId = personnelLeaveForm.EmployeeId,
+                ApplicationDate = application,
+                StatusId = 1,
+                LeaveType = personnelLeaveForm.LeaveType,
+                StartDate = personnelLeaveForm.StartDate,
+                EndDate = personnelLeaveForm.EndDate,
+                StartTime = personnelLeaveForm.StartTime,
+                EndTime = personnelLeaveForm.EndTime,
+                Proxy = personnelLeaveForm.Proxy,
+                AuditManerger = personnelLeaveForm.AuditManerger,
+                TotalTime = personnelLeaveForm.TotalTime,
+                Reason = personnelLeaveForm.Reason,
+
+            };
+            _context.PersonnelLeaveForms.Add(insert);
+            _context.SaveChanges();
         }
 
-   
+        //主管申請請假
+        // POST: api/PersonnelLeaveForms/manager
+        [HttpPost("manager")]
+        public void PostManagerLeaveForm([FromBody] PersonnelLeaveForm personnelLeaveForm)
+        {
+            var application = DateTime.Now.ToString("yyyy-MM-dd");
+
+            PersonnelLeaveForm insert = new PersonnelLeaveForm
+            {
+                EmployeeId = personnelLeaveForm.EmployeeId,
+                ApplicationDate = application,
+                StatusId = 6,
+                LeaveType = personnelLeaveForm.LeaveType,
+                StartDate = personnelLeaveForm.StartDate,
+                EndDate = personnelLeaveForm.EndDate,
+                StartTime = personnelLeaveForm.StartTime,
+                EndTime = personnelLeaveForm.EndTime,
+                TotalTime = personnelLeaveForm.TotalTime,
+                Reason = personnelLeaveForm.Reason
+
+            };
+            _context.PersonnelLeaveForms.Add(insert);
+            _context.SaveChanges();
+        }
+        //public async Task<ActionResult<PersonnelLeaveForm>> PostPersonnelLeaveForm(PersonnelLeaveForm personnelLeaveForm)
+        //{
+        //    _context.PersonnelLeaveForms.Add(personnelLeaveForm);
+        //    await _context.SaveChangesAsync();
+
+        //    return CreatedAtAction("GetPersonnelLeaveForm", new { id = personnelLeaveForm.LeaveId }, personnelLeaveForm);
+        //}
+
+
 
         //Delete被退件之採購申請單(父子資料同時刪除)
         // Delete: api/PersonnelLeaveForms/buyrej/5
