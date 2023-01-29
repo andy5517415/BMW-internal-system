6ea3c1f759964ce41e7407853242e4e82a8c2f84
andy5517415 <andy5517415@gmail.com>
2023-01-29 18:04:03 +0800

    [調整] 版面調整

--- InternalSystem/Models/MSIT44Context.cs
+++ InternalSystem/Models/MSIT44Context.cs
@@ -296,6 +296,8 @@ namespace InternalSystem.Models
 
                 entity.ToTable("PC_Application");
 
+                entity.Property(e => e.OrderId).ValueGeneratedNever();
+
                 entity.Property(e => e.Comment).HasMaxLength(200);
 
                 entity.Property(e => e.Date).HasColumnType("date");
@@ -332,6 +334,8 @@ namespace InternalSystem.Models
                 entity.HasKey(e => new { e.OrderId, e.ProductId });
 
                 entity.ToTable("PC_OrderDetails");
+
+                entity.Property(e => e.OrderId).ValueGeneratedOnAdd();
 
                 entity.Property(e => e.Goods)
                     .IsRequired()
