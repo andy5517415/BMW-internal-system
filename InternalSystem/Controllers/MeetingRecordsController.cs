e90a10becc72cfec1f9b1c09ea287bc8f728c89b
andy5517415 <andy5517415@gmail.com>
2023-02-02 14:05:41 +0800

    [調整] 會議室調整

--- InternalSystem/Controllers/MeetingRecordsController.cs
+++ InternalSystem/Controllers/MeetingRecordsController.cs
@@ -21,15 +21,28 @@ namespace InternalSystem.Controllers
             _context = context;
         }
 
-        // GET: api/MeetingRecords/1
-        [HttpGet("{BookMeetId}")]
-        public async Task<ActionResult<dynamic>> GetMeetingRecords_BookMeetId(int BookMeetId)
+        //有紀錄的才會顯示
+        // GET: api/MeetingRecords/1(部門編號)
+        [HttpGet("{depId}")]
+        public async Task<ActionResult<dynamic>> GetMeetingRecords_DepId(int depId)
         {
             var meetingBookMeetId = from a in _context.MeetingRecords
-                                    where a.BookMeetId == BookMeetId
+                                    join b in _context.MeetingReserves on a.BookMeetId equals b.BookMeetId
+                                    join c in _context.MeetingRooms on b.MeetPlaceId equals c.MeetingPlaceId
+                                    join d in _context.PersonnelDepartmentLists on b.DepId equals d.DepartmentId
+                                    join e in _context.PersonnelProfileDetails on b.EmployeeId equals e.EmployeeId
+                                    where b.DepId == depId
+                                    orderby b.Date, b.StartTime
                                     select new
                                     {
-                                        BookMeetId = a.BookMeetId,
+                                        BookId = a.BookMeetId,
+                                        MeetPlace = c.MeetingRoom1,
+                                        Date = b.Date,
+                                        Dependent = d.DepName,
+                                        EmployeeName = e.EmployeeName,
+                                        StartTime = b.StartTime,
+                                        EndTime = b.EndTime,
+                                        MeetType = b.MeetType,
                                     };
 
             if (meetingBookMeetId == null)
@@ -42,7 +55,7 @@ namespace InternalSystem.Controllers
                 return await meetingBookMeetId.ToListAsync();
             }
         }
-        
+
         //全部查詢
         public async Task<ActionResult<IEnumerable<MeetingRecord>>> GetMeetingRecords()
         {
@@ -136,16 +149,16 @@ namespace InternalSystem.Controllers
             MeetingRecord insert = new MeetingRecord
             {
                 BookMeetId = BookMeetId,
-                MeetPresident= b.MeetPresident,
-                Rcorder= b.Rcorder,
-                Participater= b.Participater,
-                ShouldAttend= b.ShouldAttend,
-                Attend= b.Attend,
-                NoAttend= b.NoAttend,
-                NoAttendPerson= b.NoAttendPerson,
-                Date= DateTime.Now,
-                Agenda=b.Agenda,
-                Record= b.Record,
+                MeetPresident = b.MeetPresident,
+                Rcorder = b.Rcorder,
+                Participater = b.Participater,
+                ShouldAttend = b.ShouldAttend,
+                Attend = b.Attend,
+                NoAttend = b.NoAttend,
+                NoAttendPerson = b.NoAttendPerson,
+                Date = DateTime.Now,
+                Agenda = b.Agenda,
+                Record = b.Record,
             };
 
             _context.MeetingRecords.Add(insert);
