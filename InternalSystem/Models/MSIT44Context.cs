ea3ca2697731518a6a24c3bbdf0e844dad630dc0
珈慶 林 <adsf8564@gmail.com>
2023-02-03 15:17:55 +0800

    修改部分資料

--- InternalSystem/Models/MSIT44Context.cs
+++ InternalSystem/Models/MSIT44Context.cs
@@ -52,7 +52,7 @@ namespace InternalSystem.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.\\SQLEXPRESS;Database=MSIT44;Integrated Security=True;");
+                optionsBuilder.UseSqlServer("Server=10.0.104.99\\L27\\SQLEXPRESS,1433;Database=MSIT44;Integrated Security=false;User ID = Lin;Password= 8564;");
             }
         }
 
@@ -140,7 +140,6 @@ namespace InternalSystem.Models
                 entity.HasOne(d => d.Employee)
                     .WithMany(p => p.BusinessOrders)
                     .HasForeignKey(d => d.EmployeeId)
-                    .OnDelete(DeleteBehavior.ClientSetNull)
                     .HasConstraintName("FK_BusinessOrder_Personnel個人資料");
             });
 
@@ -698,6 +697,7 @@ namespace InternalSystem.Models
                 entity.Property(e => e.Dispose).HasMaxLength(2000);
 
                 entity.Property(e => e.EndTime)
+                    .IsRequired()
                     .HasMaxLength(10)
                     .IsUnicode(false);
 
@@ -728,10 +728,12 @@ namespace InternalSystem.Models
                     .HasMaxLength(500);
 
                 entity.Property(e => e.EndTime)
+                    .IsRequired()
                     .HasMaxLength(10)
                     .IsUnicode(false);
 
                 entity.Property(e => e.StartTime)
+                    .IsRequired()
                     .HasMaxLength(10)
                     .IsUnicode(false);
 
